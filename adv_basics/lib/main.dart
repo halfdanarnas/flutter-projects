144c84004abf6d76fd7b86b8c5d4f10f5b78567b
halfdanarnas <160792198+halfdanarnas@users.noreply.github.com>
2024-04-12 19:54:54 +0000

    .

--- adv_basics/lib/main.dart
+++ adv_basics/lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+     MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body:  Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 53, 21, 109),
+                Color.fromARGB(255, 130, 57, 255),
+              ],
+              begin:Alignment.topLeft,
+              end: Alignment.bottomRight
+            ),
+            ),
+          child: const StartScreen(),
+          ),
       ),
     ),
   );
--- adv_basics/lib/start_screen.dart
+++ adv_basics/lib/start_screen.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
-  
+
 @override
 Widget build(context){
-return const Text('Start Screen');
+return  Center(
+  child: Column(children: [
+Image.asset('assets/images/quiz-logo.png'),
+  ],
+  ),
+);
 }
 }
