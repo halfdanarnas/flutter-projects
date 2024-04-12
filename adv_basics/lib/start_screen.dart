2eb8760b3e0f76c6941bca6dcfeb0250c32326aa
halfdanarnas <160792198+halfdanarnas@users.noreply.github.com>
2024-04-12 21:58:10 +0000

    .

--- adv_basics/lib/start_screen.dart
+++ adv_basics/lib/start_screen.dart
@@ -8,8 +8,37 @@ Widget build(context){
 
 return Center(
   child: Column(
+    mainAxisSize: MainAxisSize.min,
     children: [
-    Image.asset('assets/images/quiz-logo.png'),
+     Image.asset(
+        'assets/images/quiz-logo.png', 
+      width: 300,
+      color: const Color.fromARGB(148, 255, 255, 255),
+      ),
+  // Opacity(
+  //   opacity: 0.5,
+  //   child: Image.asset(
+  //   'assets/images/quiz-logo.png', 
+  //   width: 300,
+  //   ),
+  // ),
+    const SizedBox(height: 80,),
+    const Text(
+      'Learn Flutter the fun way!', 
+      style: TextStyle(
+        color: Color.fromARGB(150, 162, 136, 185),
+        fontSize: 24
+    ),
+    ),
+   const SizedBox(height: 30),
+   OutlinedButton.icon(
+    onPressed: () {}, 
+    style: OutlinedButton.styleFrom(
+      foregroundColor: Colors.white
+    ),
+    icon: const Icon(Icons.arrow_right_alt),
+   label: const Text('Start quiz'),
+   )
   ],
   ),
   );
