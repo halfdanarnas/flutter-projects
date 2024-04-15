14f9bbebbd992a2b3f2f89c293b6787201a730a5
halfdanarnas <160792198+halfdanarnas@users.noreply.github.com>
2024-04-15 17:27:49 +0000

    .

--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -1,3 +1,4 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -14,21 +15,33 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+ List<String> selectedAnswers = [];
 var activeScreen = 'start-screen';
 
-
-  void switchScreen() {
+ void switchScreen() {
     setState(() {
       activeScreen = 'questions-screen';
     });
   }
 
+void chooseAnswer(String answer) {
+  selectedAnswers.add(answer);
+
+  if (selectedAnswers.length == questions.length) {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'start-screen';
+    });
+  }
+}
   @override
   Widget build(context) {
     Widget screenWidget =  StartScreen(switchScreen);
 
     if (activeScreen == 'questions-screen') {
-      screenWidget = const QuestionsScreen();
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer ,
+        );
     }      
         
     return
