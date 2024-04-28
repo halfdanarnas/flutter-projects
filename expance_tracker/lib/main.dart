424c6fceaeee8e084b26c7ed0bc88a0aae224d15
halfdanarnas <160792198+halfdanarnas@users.noreply.github.com>
2024-04-28 17:48:37 +0000

    .

---
+++ expance_tracker/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:expance_tracker/expenses.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
