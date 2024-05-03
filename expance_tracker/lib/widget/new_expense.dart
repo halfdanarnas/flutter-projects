6ef3ccf9ea0254cc84c23f86520dcb6b6527f3e2
halfdanarnas <160792198+halfdanarnas@users.noreply.github.com>
2024-05-03 14:45:58 +0000

    .

--- expance_tracker/lib/widget/new_expense.dart
+++ expance_tracker/lib/widget/new_expense.dart
@@ -1,5 +1,10 @@
+import 'package:expance_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+
+final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget {
  const NewExpense ({super.key});
@@ -13,15 +18,20 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
  final _titleController = TextEditingController();
 final _amountController = TextEditingController();
+ DateTime? _selectDate;
 
-void _presentDatePicker () {
+void _presentDatePicker () async {
   final now = DateTime.now();
   final firstDate = DateTime(now.year - 1, now.month, now.day);
-  showDatePicker(
+  final pickedDate = await showDatePicker(
     context: context,
     initialDate: now,
     firstDate: firstDate, 
-    lastDate: now);
+    lastDate: now,
+    );
+    setState(() {
+      _selectDate = pickedDate;
+    });
 }
 
  @override
@@ -61,7 +71,11 @@ void _presentDatePicker () {
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment:CrossAxisAlignment.center,
               children: [
-                const Text('Selected Date'),
+                Text(
+                   _selectDate == null
+                    ?'No Date selected' 
+                    : formatter.format(_selectDate!),
+                    ),
                 IconButton(
                   onPressed: _presentDatePicker,
                   icon: const Icon
