aa8af6f661552f4fad352e2af3a0a19eca713ec1
halfdanarnas <160792198+halfdanarnas@users.noreply.github.com>
2024-05-29 23:18:21 +0000

    sound to all question

--- adv_basics/lib/data/questions.dart
+++ adv_basics/lib/data/questions.dart
@@ -3,49 +3,48 @@ import 'package:adv_basics/models/quiz_question.dart';
 const questions = [
   QuizQuestion(
     'Hvaða tónbil er þetta?',
-    ['Perfect fifth', 'Perfect fourth', 'Minor second', 'Major second'],
+    ['stækkuð 4und', 'hrein 4und', 'lítil 3und', 'stór 2und'],
     audioFilePath: 'test-sound.mp3',
   ),
   QuizQuestion('Hvaða tónbil er þetta?', [
-    'hrein 8und',
-    'hrein 5und',
     'stór 6und',
+    'hrein 5und',
+    'stór 7und',
     'lítil 6und',
   ], audioFilePath: 'test-sound2.mp3'),
   QuizQuestion(
     'Hvaða tónbil er þetta?',
     [
-      'lítil 3und',
+      'hrein 5und',
       'lítil 2und',
-      'stækkuð 4und/minnkuð 5und(tónskratti)',
+      'stækkuð 4und',
+      'lítil 3und',
+    ], audioFilePath: 'test-sound3.mp3'),
+  QuizQuestion(
+    'Hvaða tónbil er þetta?',
+    [
       'lítil 7und',
-    ],
+      'stór 3und',
+      'stór 2und',
+      'hrein 8und',
+    ], audioFilePath: 'test-sound4.mp3'
   ),
   QuizQuestion(
     'Hvaða tónbil er þetta?',
     [
-      'hrein 8und',
-      'stór 3und',
-      'stór 2und',
-      'stór 6und',
-    ],
+      'lítil 6und',
+      'lítil 2und',
+      'hrein 4und',
+      'stór 7und',
+    ], audioFilePath: 'test-sound5.mp3'
   ),
   QuizQuestion(
     'Hvaða tónbil er þetta?',
     [
       'hrein 5und',
-      'stór 6und',
-      'hrein 4und',
-      'stór 7und',
-    ],
-  ),
-  QuizQuestion(
-    'Hvaða tónbil er þetta?',
-    [
-      'stækkuð 4und/minnkuð 5und(tónskratti)',
       'stór 2und',
       'lítil 2und',
       'lítil 7und',
-    ],
+    ], audioFilePath: 'test-sound6.mp3'
   ),
 ];
