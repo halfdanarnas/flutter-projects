07fc5a416091941dd9e498a1ab17c5f67a1052ec
halfdanarnas <160792198+halfdanarnas@users.noreply.github.com>
2024-06-08 14:23:45 +0000

    image result screen

--- adv_basics/lib/results_screen.dart
+++ adv_basics/lib/results_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/questions_summary/questions_summary.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -42,50 +41,88 @@ class ResultsScreen extends StatelessWidget {
 
     final bool allCorrect = numCorrectQuestions == numTotalQuestions;
 
-    return SizedBox(
-      width: double.infinity,
-      child: Container(
-        margin: const EdgeInsets.all(40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (allCorrect)
-              Text(
-                'Til hamingju þú svaraðir öllum spurningum rétt!',
-                style: GoogleFonts.abrilFatface(
-                  color: Color.fromARGB(255, 31, 127, 31),
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              )
-            else
-              Text(
-                'Þú svaraðir $numCorrectQuestions af $numTotalQuestions spurningum rétt!',
-                style: GoogleFonts.lato(
-                  color: const Color.fromARGB(255, 255, 255, 255),
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 195, 6, 6),
+                  Color.fromARGB(255, 0, 0, 0),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
-            const SizedBox(
-              height: 30,
             ),
-            QuestionsSummary(summaryData),
-            const SizedBox(
-              height: 20,
-            ),
-            TextButton.icon(
-              onPressed: onRestart,
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
+          ),
+          SingleChildScrollView(
+            child: SizedBox(
+              width: double.infinity,
+              child: Container(
+                margin: const EdgeInsets.all(40),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (allCorrect)
+                      Column(
+                        children: [
+                          Text(
+                            'Til hamingju þú svaraðir öllum spurningum rétt!',
+                            style: GoogleFonts.abrilFatface(
+                              color: const Color.fromARGB(255, 31, 127, 31),
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(height: 20),
+                          Image.asset(
+                            'assets/images/ronburgundy.png', // Replace with your image asset path
+                            height: 150, // Adjust the height of the image
+                          ),
+                        ],
+                      )
+                    else
+                      Column(
+                        children: [
+                          Text(
+                            'Þú svaraðir $numCorrectQuestions af $numTotalQuestions spurningum rétt!',
+                            style: GoogleFonts.lato(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(height: 20),
+                          Image.asset(
+                            'assets/images/ron2.png', // Replace with your image asset path
+                            height: 150, // Adjust the height of the image
+                          ),
+                        ],
+                      ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    QuestionsSummary(summaryData),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextButton.icon(
+                      onPressed: onRestart,
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                      ),
+                      icon: const Icon(Icons.refresh),
+                      label: const Text('Gerðu aftur!'),
+                    ),
+                  ],
+                ),
               ),
-              icon: const Icon(Icons.refresh),
-              label: const Text('Gerðu aftur!'),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
